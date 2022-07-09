0e5ed5ac9ba3a1390a54fb685454f2196ca9a40a
ocemas <108965394+ocemas@users.noreply.github.com>
2022-07-09 10:08:23 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /app/config.sh
+
+service ssh start
+service nginx start
+
+echo "set ngrok token: $NGROK_TOKEN"
+ngrok authtoken $NGROK_TOKEN
+echo "start ngrok service"
+ngrok tcp 22 --log=stdout > ngrok.log
+echo "IP Address :" && curl --silent --show-error ipconfig.io
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
